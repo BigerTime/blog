7627471facc7675675973f4523f4cd971ad9c5ff
lianxiaozhuang <1176245159@qq.com>
2017-11-10 22:08:19 +0800

    js

---
+++ js/lxz.js
@@ -0,0 +1,11 @@
+;(function(){
+  
+  var blog = function(){
+    this.author = "lianxiaozhuang";
+    this.init();
+  }
+  blog.prototype.init = function(){
+    console.log("hello github")
+  }
+
+}())
